8a12c6537dba80a061ea8f5f0129a0fb8f562826
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-10-18 09:38:11 +0200

    Project initializing

---
+++ src/test/java/technobel/gedev/HotelsJavava/HotelsJavavaApplicationTests.java
@@ -0,0 +1,13 @@
+package technobel.gedev.HotelsJavava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsJavavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
